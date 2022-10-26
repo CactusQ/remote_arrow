946855fa0a7dd7897bb23a8a0477f8dd49a59cec
Long Nguyen <long.nguyen@jje-development.com>
2022-10-25 23:11:06 -0700

    init

---
+++ RemoteDataset.py
@@ -0,0 +1,127 @@
+import argparse
+import cmd
+from fileinput import filename
+import sys
+import os
+
+import pyarrow as pa
+import pyarrow.flight as paf
+import pyarrow.parquet as pq
+import pyarrow.csv as csv
+import types
+
+import inspect
+
+class RemoteDataset:
+    DEFAULT_LOCALHOST = "localhost:5005"
+    DELIMITER = "$$$"
+
+    def __init__(self, filepath=None, hostname=DEFAULT_LOCALHOST):
+        self.host, self.port = hostname.split(':')
+        self.client = paf.FlightClient(f"grpc+tcp://{self.host}:{self.port}")
+        self.descriptor = None
+        self.table = None
+        self.filename = None
+        self.id = None
+
+        if not filepath:
+            print(f"No file path given. Connect to one of the available flights:")
+            self.list_flights()
+
+        else:
+            filename, fileext = os.path.basename(filepath).split('.')
+
+            # Parse file to ArrowTable and upload to server
+            if fileext == 'csv':
+                self.table = csv.read_csv(filepath)
+            elif fileext == 'parquet':
+                self.table = pq.read_table(filepath)
+            if self.table:
+                self.filename = filename
+                self.descriptor = paf.FlightDescriptor.for_path(filename)
+                writer, _ = self.client.do_put(self.descriptor, self.table.schema)
+                writer.write_table(self.table)
+                writer.close()
+
+                # Get id 
+                self.id = len(list(self.client.list_flights()))-1
+                # Override local function calls with remote calls        
+                self.override_functions()
+                print(f"Created RemoteDataset \'{self.filename}\' (id: {self.id}) on server: {hostname}")
+                print('Total rows: ', len(self.table))
+
+            else:
+                print(f"Could not create RemoteDataset on server: {hostname}")
+                print("Unknown file format: ", fileext)
+                print("Use .csv or .parquet")
+
+    def connect(self, flight_id):
+        for id, flight in enumerate(self.client.list_flights()):
+            if id == flight_id:
+                self.descriptor = flight.descriptor
+                self.id = flight_id
+                print("Successfully connected to flight: ", self.descriptor)
+
+                # Override local function calls with remote calls        
+                self.override_functions()
+                return
+        print(f"ERROR: flight_id {id} not found.")
+        return
+        
+    def list_flights(self):
+        print('Flights\n=======')
+        for id, flight in enumerate(self.client.list_flights()):
+            print("Flight_ID: ", id)
+            descriptor = flight.descriptor
+            if descriptor.descriptor_type == paf.DescriptorType.PATH:
+                print("Path:", descriptor.path)
+            elif descriptor.descriptor_type == paf.DescriptorType.CMD:
+                print("Command:", descriptor.command)
+            else:
+                print("Unknown descriptor type")
+
+            print("Total records:", end=" ")
+            if flight.total_records >= 0:
+                print(flight.total_records)
+            else:
+                print("Unknown")
+
+            print("Total bytes:", end=" ")
+            if flight.total_bytes >= 0:
+                print(flight.total_bytes)
+            else:
+                print("Unknown")
+            print("Number of endpoints:", len(flight.endpoints))
+            print("Schema:")
+            print(flight.schema)
+            print('---')
+    
+    def list_actions(self):
+        print('\nActions\n=======')
+        for action in self.client.list_actions():
+            print("Type:", action.type)
+            print("Description:", action.description)
+            print('---')
+
+    # Override local ArrowTable methods to their RPC version
+    def override_functions(self):
+        for attr_name in dir(pa.Table):
+            attr = getattr(pa.Table, attr_name)
+            if callable(attr) and not attr_name.startswith("_"):
+                super().__setattr__(attr_name, self.create_rpc_call(attr_name))
+
+    # Transforms a given ArrowTable method to an equivalent remote procedure call
+    def create_rpc_call(self, attr_name):
+        def rpc(*args, **kwarg):
+            args_dict = {i: args[i] for i in range(len(args))}
+            cmd_string = self.DELIMITER.join([str(self.id), attr_name, str(args_dict), str(kwarg)])
+            descriptor = paf.FlightDescriptor.for_command(cmd_string)
+            info = self.client.get_flight_info(descriptor)
+            for endpoint in info.endpoints:
+                for location in endpoint.locations:
+                    get_client = paf.FlightClient(location)
+                    reader = get_client.do_get(endpoint.ticket)
+                    if descriptor.descriptor_type == paf.DescriptorType.CMD:
+                        return reader.read_all()
+
+        return rpc
---
+++ server.py
@@ -0,0 +1,198 @@
+import argparse
+import ast
+
+import threading
+import time
+import os
+
+import pyarrow as pa
+import pyarrow.flight as paf
+import pyarrow.parquet as pq
+
+from RemoteDataset import RemoteDataset
+
+from ast import literal_eval as make_tuple
+
+class FlightServer(paf.FlightServerBase):
+    def __init__(self, host="localhost", location=None,
+                 tls_certificates=None, verify_client=False,
+                 root_certificates=None, auth_handler=None):
+
+        super(FlightServer, self).__init__(
+            location, auth_handler, tls_certificates, verify_client,
+            root_certificates)
+        
+        # Create mapping from id to key for readability
+        self.id_to_key = {}
+        self.id_counter = 0
+
+        self.flights = {}
+        for filepath in os.listdir(os.getcwd()):
+             if filepath.endswith(".parquet"):
+                # Generate descriptor from file name and cast to a key
+                filename = os.path.basename(filepath).split('.')[0]
+                desc = pa.flight.FlightDescriptor.for_path(filename)
+                key = self.descriptor_to_key(desc)
+                self.flights[key] = pq.read_table(filename+'.parquet', memory_map=True)
+                self.store_key(key)      
+                print("Loaded from disk: " + filename+'.parquet')
+        print("List of flights:")
+        print(self.id_to_key)
+        self.host = host
+
+    @classmethod
+    def descriptor_to_key(self, descriptor):
+        return (descriptor.descriptor_type.value, descriptor.command,
+                tuple(descriptor.path or tuple()))
+    
+    def store_key(self, key):
+        self.id_to_key[str(self.id_counter)] = key
+        self.id_counter += 1  
+
+    def _make_flight_info(self, key, descriptor, table):
+        location = paf.Location.for_grpc_tcp(
+        self.host, self.port)
+        endpoints = [paf.FlightEndpoint(repr(key), [location]), ]
+        mock_sink = pa.MockOutputStream()
+        stream_writer = pa.RecordBatchStreamWriter(
+            mock_sink, table.schema)
+        stream_writer.write_table(table)
+        stream_writer.close()
+        data_size = mock_sink.size()
+        return paf.FlightInfo(table.schema,
+                                         descriptor, endpoints,
+                                         table.num_rows, data_size)
+
+    def list_flights(self, context, criteria):
+        for id, key in self.id_to_key.items():
+            table = self.flights[key]
+            if key[1] is not None:
+                descriptor = paf.FlightDescriptor.for_command(key[1])
+            else:
+                descriptor = paf.FlightDescriptor.for_path(*key[2])
+            yield self._make_flight_info(key, descriptor, table)
+
+
+    def get_flight_info(self, context, descriptor):
+        key = FlightServer.descriptor_to_key(descriptor)
+        if descriptor.descriptor_type == paf.DescriptorType.CMD \
+            and key not in self.flights:
+                if not self.do_command(key, descriptor):
+                    return
+
+        if key in self.flights:
+            table = self.flights[key]
+            return self._make_flight_info(key, descriptor, table)
+                       
+        raise KeyError('Flight not found.')
+
+    def do_command(self, new_key, descriptor) -> bool:
+        decoded_str = descriptor.command.decode("utf-8")
+        id, method, strargs, strkwargs = decoded_str.split(RemoteDataset.DELIMITER)
+
+        args = tuple(ast.literal_eval(strargs).values())
+
+        # Flatten tuples that only have 1 element
+        if len(args) == 0:
+            args = None
+        elif len(args) == 1:
+            args = args[0]
+
+        kwargs = ast.literal_eval(strkwargs)
+        kwargs = kwargs if len(kwargs) > 0 else None
+
+        print("str: ", decoded_str)
+        print("strargs:", strargs)
+        print("strkwargs:", strkwargs)
+        print("args:", args)
+        print("kwargs:", kwargs)
+        key = self.id_to_key[str(id)]
+
+        # TODO: can this be done more elegantly?
+        if args and kwargs:
+            result = getattr(self.flights[key], method)(args, kwargs)
+        elif args and kwargs is None:
+            result = getattr(self.flights[key], method)(args)
+        elif kwargs and args is None:
+            result = getattr(self.flights[key], method)(kwargs)
+        else:
+            result = getattr(self.flights[key], method)()
+
+        if type(result) == pa.Table:
+            self.flights[new_key] = result
+            self.store_key(new_key)
+            return True
+        else:
+            return False
+            
+
+    def do_put(self, context, descriptor, reader, writer):
+        #print("DESCRIPTOR:", descriptor)
+        key = FlightServer.descriptor_to_key(descriptor)
+        print("Received put command with key:", key)
+        self.flights[key] = reader.read_all()
+        self.store_key(key)
+        filepath = key[2][0].decode("utf-8")+'.parquet'
+        pq.write_table(self.flights[key], filepath)
+        #with pq.ParquetWriter(filepath, reader.schema) as writer:
+        #    for chunk in reader:
+        #        writer.write_table(pa.Table.from_batches([chunk.data]))
+        #self.flights[key] = reader.read_all()
+        print("Writing to " + filepath)
+#        writer.close()
+
+    def do_get(self, context, ticket):
+        key = ast.literal_eval(ticket.ticket.decode())
+        if key not in self.flights:
+            return None
+        elif key[0] == 2:
+            return paf.RecordBatchStream(self.flights[key])
+
+        if len(key[2]) == 0: return
+        filename = key[2][0].decode("utf-8")
+        reader = pq.ParquetFile(filename+'.parquet', memory_map=True)
+        return paf.GeneratorStream(
+            reader.schema_arrow, reader.iter_batches())
+
+    def list_actions(self, context):
+        return [
+            ("clear", "Clear the stored flights."),
+            ("shutdown", "Shut down this server."),
+        ]
+
+    def do_action(self, context, action):
+        if action.type == "clear":
+            raise NotImplementedError(
+                "{} is not implemented.".format(action.type))
+        elif action.type == "healthcheck":
+            pass
+        elif action.type == "shutdown":
+            yield paf.Result(pa.py_buffer(b'Shutdown!'))
+            # Shut down on background thread to avoid blocking current
+            threading.Thread(target=self._shutdown).start()
+        else:
+            raise KeyError("Unknown action {!r}".format(action.type))
+        
+    def _shutdown(self):
+        """Shut down after a delay."""
+        print("Server is shutting down...")
+        time.sleep(2)
+        self.shutdown()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="localhost",
+                        help="Address or hostname to listen on")
+    parser.add_argument("--port", type=int, default=5005,
+                        help="Port number to listen on")
+    args = parser.parse_args()
+    scheme = "grpc+tcp"
+
+    location = "{}://{}:{}".format(scheme, args.host, args.port)
+    server = FlightServer(args.host, location)
+    print("Server online: ", location)
+    server.serve()
+
+if __name__ == '__main__':
+    main()
